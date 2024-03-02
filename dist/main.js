bbb6320c4ac9cf3600bb96e3e0c5820ce9ba4bb5
syedubaid <syedabuubaid@gmail.com>
2024-03-03 02:47:11 +0530

    first commit

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var e,t;t=(e=document.createElement("h1")).appendChild(document.createElement("section")),document.body.appendChild(t),e.textContent="hello this is a test"})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+function render(){
+    var demo=document.createElement("h1")
+    var test=demo.appendChild(document.createElement("section"))
+    document.body.appendChild(test)
+
+    demo.textContent="hello this is a test"
+}
+
+render()
